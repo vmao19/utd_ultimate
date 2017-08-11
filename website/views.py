7ce0104f1f95ccab745d584663a603e733f92070
Victor Mao <victormao98@gmail.com>
2017-08-10 19:08:13 -0500

    Added About, Gallery, and Contact Us Pages

--- website/views.py
+++ website/views.py
@@ -6,28 +6,27 @@ def index(request):
 
 def woofabout(request):
 	return render(request, 'woof/about.html')
-	
 def woofroster(request):
 	return render(request, 'woof/roster.html')
-#def woofgallery(request):
-#	return render(request, 'woof/gallery.html')
-#def woofcontactus(request):
-#	return render(request, 'woof/contactus.html')
+def woofgallery(request):
+	return render(request, 'woof/gallery.html')
+def woofcontactus(request):
+	return render(request, 'woof/contactus.html')
 
-#def barkabout(request):
-#	return render(request, 'bark/about.html')
+def barkabout(request):
+	return render(request, 'bark/about.html')
 def barkroster(request):
 	return render(request, 'bark/roster.html')
-#def barkgallery(request):
-#	return render(request, 'bark/gallery.html')
-#def barkcontactus(request):
-#	return render(request, 'bark/contactus.html')
+def barkgallery(request):
+	return render(request, 'bark/gallery.html')
+def barkcontactus(request):
+	return render(request, 'bark/contactus.html')
 
-#def monstarsabout(request):
-#	return render(request, 'monstars/about.html')
+def monstarsabout(request):
+	return render(request, 'monstars/about.html')
 def monstarsroster(request):
 	return render(request, 'monstars/roster.html')
-#def monstarsgallery(request):
-#	return render(request, 'monstars/gallery.html')
-#def monstarscontactus(request):
-#	return render(request, 'monstars/contactus.html')
+def monstarsgallery(request):
+	return render(request, 'monstars/gallery.html')
+def monstarscontactus(request):
+	return render(request, 'monstars/contactus.html')
